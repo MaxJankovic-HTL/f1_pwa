d205112bc22623be4f354a7779a54d6f6c0b4081
MaxJankovic-HTL <104189823+MaxJankovic-HTL@users.noreply.github.com>
2025-02-11 17:59:12 +0100

    Tabelle

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
 import { useMyStore } from '../stores/myStore';
+import { ref } from 'vue';
 
-const myStore = useMyStore();
+const store = useMyStore();
+
+store.fetchdata();
+
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Name',
+    align: 'left',
+    field: 'name',
+  },
+  { name: 'breitengrad', align: 'center', label: 'Breitengrad', required: true, field: 'breitengrad' },
+  { name: 'laengengrad', label: 'Laengengrad', required: true, field: 'laengengrad' },
+  { name: 'img', label: 'Bilder', field: 'img' },
+  { name: 'baujahr', label: 'Baujahr', field: 'baujahr' }
+]
+
 </script>
 
 <template>
-  <div class="column items-center q-mt-md"><span class="text-h4">Home</span><br /><span>{{ myStore.message }}</span></div>
+  <div class="column items-center q-mt-md">
+    <q-table
+      title="Treats"
+      :rows="store.track"
+      :columns="columns"
+      row-key="id"
+    />
+  </div>
 </template>
