c9dcb46f3ae76d2f54d15482d37b4679ec5ae2df
MaxJankovic-HTL <104189823+MaxJankovic-HTL@users.noreply.github.com>
2025-03-12 19:32:11 +0100

    REST server, all CRUD operations funktioniert

--- server/src/model/trackModel.js
+++ server/src/model/trackModel.js
@@ -12,18 +12,18 @@ export const getAll = async () => {
 };
 
 export const delTrack = async (id) => {
-  const { rows } = await query('DELETE FROM tracks WHERE id = $1;', [id]);
+  const { rows } = await query('DELETE FROM tracks WHERE id = $1 RETURNING *;', [id]);
   return rows;
 };
 
 export const updateTrackName = async (id, name) => {
-  const { rows } = await query("UPDATE tracks SET name = '$2' WHERE id = $1;", [id, name]);
+  const { rows } = await query('UPDATE tracks SET name = $2 WHERE id = $1 RETURNING *;', [id, name]);
   return rows;
 };
 
 export const createTrack = async (name, bg, lg, bj) => {
   const { rows } = await query(
-    "INSERT INTO tracks(name, breitengrad, laengengrad, baujahr) VALUES ('$1', $2, $3, '$4');",
+    'INSERT INTO tracks(name, breitengrad, laengengrad, baujahr) VALUES ($1, $2, $3, $4);',
     [name, bg, lg, bj],
   );
   return rows;
