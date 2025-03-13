25c0662613576fcb005d9eb3a93ca9f6bc2a2bf4
MaxJankovic-HTL <104189823+MaxJankovic-HTL@users.noreply.github.com>
2025-03-13 13:35:53 +0100

    aboutview console.log

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="column items-center q-mt-md text-center">
     <span class="text-h4">Impressum</span>
-    <span class="text-subtitle1">Implementiert von Maximilian Jankovic &copy; HTL Wien West, 2024</span>
+    <span class="text-subtitle1"
+      >Implementiert von Maximilian Jankovic &copy; HTL Wien West, 2024</span
+    >
   </div>
 </template>
+
+<script setup>
+console.log('Maximilian Jankovic');
+</script>
